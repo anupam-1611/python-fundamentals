342f75b0be297e233d26beded557e77bdbf10106
Anupam Dutta <anupamduttahcs@gmail.com>
2026-08-14 12:14:36 +0530

    Add string slicing and indexing examples

---
+++ Slicing and Indexing.py
@@ -0,0 +1,16 @@
+code = "AnupamDutta-22"
+
+print(code[0]) #"A" beacause indexing starts with 0 
+
+print(code[-1]) #2 because starts from the back of the string
+
+print(code[0:6]) # Starts from 0 and ends in 5th Index
+
+print(code[-5:]) #starts from -2 i.e, T and ends at the last cuz no value given
+
+date  = 2026-05-29
+
+print(date[0:4], date[5:7], date[-5:])
+
+print(code[0:10:2])
+# starts at 0 and ends at 10th posi and also jumps 2 steps 
