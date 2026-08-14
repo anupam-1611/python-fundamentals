e9537ae31cd0ea68632bfc5ab30dc854c542252f
Anupam Dutta <anupamduttahcs@gmail.com>
2026-08-14 11:11:45 +0530

    Add input for name and age with corrections
    Fix input prompts and correct variable names.

---
+++ input.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+#problem with input function is that it only takes input as strings
+
+print("My anem is : ", name)
+
+x = int(input("Enter you age: "))
+print("Your age after 10 years: ", age + 10)
+# the int functions changes the type from string to integer as well as any type you want
