83086acf1a4a5374deb6efa25bedf9e53f4e4626
Anupam Dutta <anupamduttahcs@gmail.com>
2026-08-14 11:56:58 +0530

    Add string manipulation examples in string.py
    Added string manipulation examples including date formatting, string interpolation, and CSV splitting.

---
+++ string.py
@@ -0,0 +1,12 @@
+date = "2026/05/10"
+print(date.replace("/","-"))
+#replaces certain text to another
+
+first = "Anupam"; last = "Dutta"
+print(f"{first} {last}")
+
+cssv = "Anupam,22,India"
+print(cssv.split(","))
+#creates a list
+
+print("=" *20)
