018fa9a8381c8edb2227b1f82eb256d86e8aa36b
Anupam Dutta <anupamduttahcs@gmail.com>
2026-08-14 11:19:32 +0530

    Implement revenue calculation and print example
    Add revenue calculation and example print statement

---
+++ printtt.py
@@ -0,0 +1,14 @@
+total_qty = 35
+price = 100
+revenue = price * total_qty
+print(revenue)
+
+# this print func() only shows the calculation that is stored in the variable 
+
+print("""
+Hello world
+
+prints exactly as it is written
+""")
+
+#triple quotes is used to print the written text as it is written inside the print func()
